2291f6f4ad660e68c71905f6819c3c848f7e21bf
Matěj <45061792+matejnesuta@users.noreply.github.com>
2020-01-20 17:53:16 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,37 +1,49 @@
 import tkinter as tk 
 from tkinter import messagebox
-import sys
 import random 
-import time
+import json
+
 class App(tk.Tk): 
     def __init__(self): 
         super().__init__() 
         self.title("Chess Coordinate Trainer") 
-        self.geometry("1000x1000") 
+        self.geometry("1050x900") 
         self.resizable(0, 0)
-        self.configure(background="#fba800")
+        self.configure(background="#bfbfbf")
         self.img = tk.PhotoImage(file="pixel.png") 
         self.ribbon()
         self.chessboard()
         self.souradnice = tk.StringVar()
         self.souradnice.set("...")
-        self.currentCoordinate=tk.Label(textvariable=self.souradnice, font=("Helvetica", 25))
-        self.currentCoordinate.grid(row=1,column=8, columnspan=2)
+        self.currentCoordinate=tk.Label(textvariable=self.souradnice, width="8", font=("Helvetica, 40"),borderwidth="1", relief="solid")
+        self.currentCoordinate.grid(row=1,column=8, columnspan=4)
+        self.timeDescription=tk.Label(text="Zbývající čas:", font=("Helvetica, 20"),borderwidth="1", relief="solid", padx="10")
+        self.timeDescription.grid(row=2, column=8,columnspan=2)
         self.time = tk.DoubleVar()
         self.time.set(00.00)
-        self.cas=tk.Label(textvariable=self.time, font=("Helvetica, 40"))
-        self.cas.grid(row=2,column=8, columnspan=2)
+        self.cas=tk.Label(textvariable=self.time, width="4", font=("Helvetica, 20"), borderwidth="1", relief="solid")
+        self.cas.grid(row=2,column=10, columnspan=2)
         self.gameOn=0
+        self.scoreDescription=tk.Label(text="Skóre:", font=("Helvetica, 20 "),borderwidth="1", relief="solid", padx="53")
+        self.scoreDescription.grid(row=3, column=8,columnspan=2)
         self.score=tk.IntVar()
         self.score.set(0)
-        self.scoreLabel=tk.Label(textvariable=self.score, font=("Helvetica", 25))
-        self.scoreLabel.grid(row=3, column=8, columnspan=2)
+        self.scoreLabel=tk.Label(textvariable=self.score, font=("Helvetica, 20"),borderwidth="1", relief="solid", width="4")
+        self.scoreLabel.grid(row=3, column=10, columnspan=2)
         self.index=-1
-        
+        self.player = 0
+        self.barva=tk.StringVar()
+        self.barva.set("Bílá")
+        self.pohled = tk.Label(textvariable=self.barva, width="8", font=("Helvetica, 40"),borderwidth="1", relief="solid")   
+        self.pohled.grid(row=4, column=8, columnspan=4)     
     
-    
-    def zavreni(self):
-        self.win.destroy()
+    "Tyto dve funkce určujou pohled hráče."
+    def whiteplayer(self):
+        self.player = 0
+        self.barva.set("Bílá")
+    def blackplayer(self):
+        self.player = 1
+        self.barva.set("Černá")
 
     def settings(self):
         self.win = tk.Toplevel()
@@ -41,25 +53,32 @@ class App(tk.Tk):
         self.win.resizable(0,0)
         perspektiva = tk.Label(self.win, text="Nastavení perspektivy: ")
         perspektiva.grid(row=0, column=0)
-        white=tk.Button(self.win, text="Bílá")
+        white=tk.Button(self.win, text="Bílá", command=self.whiteplayer)
         white.grid(row=0, column=1)
-        black=tk.Button(self.win, text="Černá")
+        black=tk.Button(self.win, text="Černá", command=self.blackplayer)
         black.grid(row=0, column=2)
         souradnice=tk.Label(self.win, text="Vypnutí/zapnutí souřadnic: ")
         souradnice.grid(row=1, column=0)
-        b = tk.Button(self.win, text="Okay", command=self.zavreni)
-        b.grid(row=2, column=0)
 
     def info(self):
         self.win = tk.Toplevel()
         self.win.wm_title("Window")
         self.win.grab_set()
-        l = tk.Label(self.win, text="Tato krásná věc byla vytvořena jako můj domácí úkol.")
+        l = tk.Label(self.win, text="Tato krásná věc byla vytvořena jako můj domácí úkol. \n\n “The blunders are all there on the board, waiting to be made.” - Savielly Tartakower")
         l.grid(row=0, column=0)
-
-        b = tk.Button(self.win, text="Okay", command=self.zavreni)
-        b.grid(row=1, column=0)
     
+    def leaderboard(self):
+        pass
+        """self.win = tk.Toplevel()
+        self.win.wm_title("Žebříček")
+        self.win.grab_set()
+        with open("leaderboard.json", "r") as inside:
+            data = json.load(inside)
+        print (data[0])
+        text = tk.Label(self.win, state='normal', height=20, width=60)"""
+
+    """Vykreslení šachovnice samotné. Každé políčko je vyvoláno jako tlačítko a přidáno do listu. 
+    Tlačítka vrací index od 0-63 a ten se porovnává s náhodně vygenerovaným indexem, který určuje souřadnici, jenž musí hráč najít na šachovnici."""
     def chessboard(self):     
         self.coordinates = ("A1","A2","A3","A4","A5","A6","A7","A8","B1","B2","B3","B4","B5","B6","B7","B8","C1","C2","C3","C4","C5","C6","C7","C8","D1","D2","D3","D4","D5","D6","D7","D8","E1","E2","E3","E4","E5","E6","E7","E8","F1","F2","F3","F4","F5","F6","F7","F8","G1","G2","G3","G4","G5","G6","G7","G8","H1","H2","H3","H4","H5","H6","H7","H8")
         self.buttons = []
@@ -73,7 +92,6 @@ class App(tk.Tk):
                 self.buttons.append(b)
                 self.buttons[2*8*x+y].grid(row=8-y,column=2*x)
                 i=i+1
-                print(2*8*x+y)
                 
             for y in range (0,8):
                 if y % 2 == 1:
@@ -83,29 +101,33 @@ class App(tk.Tk):
                 self.buttons.append(b)
                 self.buttons[2*8*x+y+8].grid(row=8-y,column=2*x+1)
                 i=i+1
-                print(2*8*x+y+8)
-    
+
     def game(self):
         if self.gameOn==0:
             self.gameOn=1
             self.startname.set("Storno")
-            self.timer=30.00
+            #cas je nastavenej v milisekundách, aby mezi vypršením časomíry a vyskočením okna s výsledkem nebyla 1 sekundová odezva, jelikož funkce časovače volala sama sebe po jedné sekundě a do toho odčítala proměnnou času. 
+            #1 sekunda byla dost na to, aby člověk stihl zmáčknout start a storno po sobě a rozbít tak časovač.
+            self.timer=30000
             self.index=random.randint(0,63)
             self.souradnice.set(self.coordinates[self.index])
             self.time.set(self.timer)
-            self.after(100,self.stopky)
+            self.stopky()
         else:
+            #else větev, pokud by se hráč rozhodl stronovat hru
             self.gameOn=0
             self.souradnice.set("...")
             self.score.set(0)
             self.timer=0.0
             self.time.set(00.00)
             self.startname.set("Start")
+
     def stopky(self):
         if self.timer >= 0:
-            self.time.set(self.timer)
+            if self.timer % 1000 == 0:
+                self.time.set(self.timer/1000)
             self.timer-=1
-            self.after(1000, self.stopky)
+            self.after(1, self.stopky)
         else:
             self.gameOn=0
             self.souradnice.set("...")
@@ -114,11 +136,18 @@ class App(tk.Tk):
             self.startname.set("Start")
     
     def callback(self, click):
+            print(click)
             if self.gameOn == 1:  
-                if click==self.index:
-                    self.score.set(self.score.get()+1)
-                    self.index=random.randint(0,63)
-                    self.souradnice.set(self.coordinates[self.index])
+                if self.player == 0:   
+                    if click==self.index:
+                        self.score.set(self.score.get()+1)
+                        self.index=random.randint(0,63)
+                        self.souradnice.set(self.coordinates[self.index])
+                elif self.player == 1:   
+                    if 63-click==self.index:
+                        self.score.set(self.score.get()+1)
+                        self.index=random.randint(0,63)
+                        self.souradnice.set(self.coordinates[self.index])
             else:
                 pass    
                 
@@ -127,20 +156,13 @@ class App(tk.Tk):
         self.startname.set("Start")
         self.start=tk.Button(textvariable=self.startname, image=self.img, compound=tk.LEFT, height=21, width=192, command=lambda:self.game())
         self.options=tk.Button(text="Nastavení", image=self.img, compound=tk.LEFT, height=21, width=192, command=lambda:self.settings())
-        self.vysledky=tk.Button(text="Tabulka výsledků", image=self.img, compound=tk.LEFT, height=21, width=192, )
+        self.vysledky=tk.Button(text="Tabulka výsledků", image=self.img, compound=tk.LEFT, height=21, width=192, command=self.leaderboard)
         self.about=tk.Button(text="O aplikaci", image=self.img, compound=tk.LEFT, height=21, width=192, command=lambda:self.info())
-        self.exit=tk.Button(text="Ukončit", image=self.img,compound=tk.LEFT, height=21, width=192, command=sys.exit)
-        
         self.start.grid(row=0, column=0, columnspan=2)
         self.options.grid(row=0, column=2, columnspan=2)
         self.vysledky.grid(row=0, column=4, columnspan=2)
         self.about.grid(row=0, column=6, columnspan=2)
-        self.exit.grid(row=0, column=8, columnspan=2)  
         
-
-    
-
-
 if __name__ == "__main__": 
     app = App() 
     app.mainloop()
